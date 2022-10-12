df0318ec1e0fb0f4f25dd78b4695d6ead17650f4
Huu <hoangduchuuvn@gmail.com>
2022-10-12 23:08:35 +0700

    Initial commit

---
+++ src/test/kotlin/com/developerfect/fitness/FitnessApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.developerfect.fitness
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class FitnessApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
